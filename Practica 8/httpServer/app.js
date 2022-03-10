d213393d7fc9082e452dd95c2f2b1740b7ff0d0b
Joshua <mramos29@ucol.mx>
2022-03-09 20:29:48 -0600

    practica 8 donete

--- Practica 8/httpServer/app.js
+++ Practica 8/httpServer/app.js
@@ -1,6 +1,7 @@
 var http = require('http');
-
+// Tras instalar http a través de node, ahora se puede inicializar un servidor local
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type' : 'text/plain'});
     res.end('Hello world\n');
 }).listen(1337, '127.0.0.1');
+// El servidor atenderá al puerto 1337 , con la dirección ip 127.0.0.1 y responderá la llamada con "Hello world"

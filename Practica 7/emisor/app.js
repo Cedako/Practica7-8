81ab9c29a10b56fb9324d610aae4d1a461680110
Joshua <mramos29@ucol.mx>
2022-03-09 20:27:34 -0600

    practica 7 donete

--- Practica 7/emisor/app.js
+++ Practica 7/emisor/app.js
@@ -1,13 +1,16 @@
 // var Emitter = require('./emitter');
 var Emitter = require('events')
 // Ahora se solicita "events" en vez del archivo "emitter"
+var config = require('./config')
+// Se inyecta la dependencia config
 var emtr = new Emitter();
 
-emtr.on('greet', () => {
+emtr.on(config.events.GREET, () => {
     console.log('somewhere, someone said hello.')
 });
+// Se invoca "greet" a través de config, donde su string ahora está asignada a una variable, para evitar typos.
 
-emtr.on('greet', () => {
+emtr.on(config.events.GREET, () => {
     console.log('a greeting occurred.')
 });
 
